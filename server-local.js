e191c748b760ca356be387a7c36a9526d4707cde
David Jones-Gilardi <david.gilardi@datastax.com>
2021-05-07 14:07:34 +0000

    Removed express references and simplifying functions

--- server-local.js
+++ server-local.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const server = require('./express/server')
+const server = require('./functions/server')
 
 server.listen(3000, () => console.log('Local app listening on port 3000!'))
 
-const server_basic = require('./express/server_basic')
+const server_basic = require('./functions/server_basic')
 
 server_basic.listen(3001, () => console.log('Local app listening on port 3001!'))
